aa3f64a299deed2a130638d672b97b62ea37a3eb
Aleksandr <aleksandr.sayko@gmail.com>
2015-09-29 18:42:11 +0300

    add functions: filterRange(arr, a, b) camelize(str)

--- Lesson 16/script2.js
+++ Lesson 16/script2.js
@@ -4,7 +4,8 @@
 var country = ['Russia', 'USA', 'Europe'],
     population = [100, 200],
     fruits = ['апельсин', 'банан', 'груша'], //1. Создайте массив с фруктами: апельсин, банан, груша.
-    fruits2 = ["яблоко", "апельсин", "груша", "гранат"];
+    fruits2 = ["яблоко", "апельсин", "груша", "гранат"],
+    numbers = [1, 5, 6, 3, 5, 8, 10, 4, 6];
 
 function printPopulation(country, population) {
     var i;
@@ -16,7 +17,7 @@ function printPopulation(country, population) {
 function printPopForEach(country, population) {
     country.forEach(function(item, i) {
         if (i < population.length) console.log(item, population[i]);
-    })
+    });
 }
 
 printPopulation(country, population);
@@ -41,13 +42,41 @@ printPopForEach(country, population);
     fruits2.splice(-1, 1, "ананас"); //вариант после дополнительного прочтения материала :)
 
 //Напишите код для вывода alert случайного значения из массива
-    console.log(fruits[ Math.round(Math.random() * (fruits.length - 1)) ]);
+    console.log(fruits[ Math.floor(Math.random() * fruits.length) ]);
 
 //Создайте функцию find(arr, value), которая ищет в массиве arr значение value и возвращает его номер,
 // если найдено, или -1, если не найдено.
 function find(arr, value) {
     return arr.indexOf(value);
 }
+
+
+// Создайте функцию filterRange(arr, a, b), которая принимает массив чисел arr и возвращает новый массив, 
+// который содержит только числа из arr из диапазона от a до b. То есть, проверка имеет вид a ≤ arr[i] ≤ b. 
+// Функция не должна менять arr. 
+
+function filterRange(arr, a, b) {
+    var _arr = [];
+
+    arr.forEach(function (item, i, arr) {
+            if (item >= a && item <= b) _arr.push(item);
+    }); 
+
+    return _arr;
+}
+
+console.log( filterRange(numbers, 4, 7) );
+
+// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+function camelize(str) {
+    var arr = str.split('-'),
+        i;
+
+    for (i = 1; i < arr.length; i++) {
+        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    }
+    return arr.join('');
+}   
 
 //У вас есть массив: var students = ["Илья","Greg", "Алина"];
 //Вам надо пройтись по каждому элементу этого массива и выяснить в каком элементе массива есть буквы,
@@ -56,8 +85,10 @@ function findNotRuLetters(arr) {
     var result = [],
         i;
     for (i = 0; i< arr.length; i++) {
-        if (arr[i].search(/[^А-ЯЁ][^а-яё]/) > -1) {result.push(i)}
+        if (arr[i].search(/[^А-ЯЁ]/i) > -1) {
+            result.push(i);
+        }
     }
-    return result
+    return result;
 }
 console.log(findNotRuLetters(["Илья","Greg", "Алина"]));
